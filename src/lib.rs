1db48ea6fd5981494e6e1116c8c46a484820441e
One <43485962+c-git@users.noreply.github.com>
2024-06-23 12:31:59 -0400

    Record bug found again

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,4 @@ pub mod background_worker;
 pub use app::DBV;
 
 // TODO 1: Handle bug that when `Should round new points` is checked, points colors submenu is unreachable
+// TODO 1: Color picker doesn't appear to be working even when it can be reached
